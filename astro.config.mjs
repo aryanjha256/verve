cb94c1bfd022fe34a5779f667cc06e6664999714
Aryan Kumar <aryanajha256@gmail.com>
2024-01-17 01:09:11 +0530

    Add @astrojs/mdx integration and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [tailwind({
-    applyBaseStyles: true
-  }), sitemap()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    sitemap(),
+    mdx(),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+    },
+  },
 });
