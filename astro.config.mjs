b6192de1c2faea48340f9857b8ad0606423f7ec1
Aryan Kumar <aryanajha256@gmail.com>
2024-01-17 00:40:12 +0530

    Add Astro RSS and Sitemap integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [
-    tailwind({
-      applyBaseStyles: true,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: true
+  }), sitemap()]
 });
