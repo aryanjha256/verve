e86c7710aa2dfd052fcb562897eb3016f4c2ad38
Aryan Kumar <aryanajha256@gmail.com>
2024-02-12 03:18:36 +0530

    feat: post search

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,19 @@ import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [
-    tailwind({
-      applyBaseStyles: true,
-    }),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: true
+  }), sitemap(), mdx(), react()],
   markdown: {
     shikiConfig: {
-      theme: "one-dark-pro",
-    },
-  },
+      theme: "one-dark-pro"
+    }
+  }
 });
